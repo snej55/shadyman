54db62f5893773afb0bd5c6267aa4ec2f5c61ac7
snej55 <jenskromdijk@gmail.com>
2025-06-29 16:49:36 +0100

    Add fps counter

--- src/game.cpp
+++ src/game.cpp
@@ -1,10 +1,14 @@
 #include "game.hpp"
 #include "constants.hpp"
 
+#include <sstream>
+
 void Game::init()
 {
     InitWindow(CST::SCR_WIDTH, CST::SCR_HEIGHT, CST::WIN_NAME.c_str());
     SetWindowState(FLAG_WINDOW_RESIZABLE);
+
+    SetTargetFPS(60);
 }
 
 void Game::run()
@@ -14,6 +18,11 @@ void Game::run()
         BeginDrawing();
 
         ClearBackground({0x00, 0x00, 0x00});
+
+        float frameTime {GetFrameTime() * 1000.f};
+        std::stringstream ss{};
+        ss << "Frame time: " << frameTime << " ms";
+        DrawText(ss.str().c_str(), 5, 5, 10, WHITE);
 
         EndDrawing();
     }
